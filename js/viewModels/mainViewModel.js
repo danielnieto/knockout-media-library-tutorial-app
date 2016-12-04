4564f4ba08a9984eb25c8d975fa51d3dc985a16d
Daniel Nieto <danielnieto89@gmail.com>
2016-12-04 12:25:10 -0600

    add the main viewModel which uses dataContext to populate its internal catalog
    this is the main viewModel for the app

---
+++ js/viewModels/mainViewModel.js
@@ -0,0 +1,19 @@
+var app = window.app || {};
+app.MainViewModel = (function(ko, db){
+    "use strict";
+
+    var me = {
+        catalog: [],
+        init: init
+    }
+
+    function init(){
+         db.getCatalog(function(data){
+             ko.utils.arrayForEach(data || [], function(item){
+                 me.catalog.push(new app.Media(item.ISBN, item.MediaType, item.Name));
+             });
+         });
+    }
+
+    return me;
+})(ko, app.DataContext);
