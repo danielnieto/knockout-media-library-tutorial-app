64f75c804f42d038f83cd68289d7a3337b832df6
Daniel Nieto <danielnieto89@gmail.com>
2016-12-07 23:59:02 -0600

    example of how to use two way data binding
    with an input text and an observable

--- js/viewModels/dataBindingViewModel.js
+++ js/viewModels/dataBindingViewModel.js
@@ -1,10 +1,24 @@
 var app = window.app || {};
-app.DataBindingViewModel = (function(ko){
+app.DataBindingViewModel = (function(){
     "use strict";
 
     var me = {
-        
+        message: ko.observable(''),
+        colors: ["Red", "Green", "Blue", "Yellow", "Black", "White"],
+        loadMessage: loadMessage
     }
 
+    function _init(){
+        me.message.subscribe(function(v){
+            console.log(v);
+        });
+    }
+
+    function loadMessage(){
+        me.message("My favorite color is: " + me.colors[Math.floor(Math.random() * me.colors.length)].toLowerCase());
+    }
+
+    _init();
+
     return me;
-})(ko);
+})();
