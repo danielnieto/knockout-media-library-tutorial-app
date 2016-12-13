9da2b181bb5d192fe3fb948aba2782655b8da1ae
Daniel Nieto <danielnieto89@gmail.com>
2016-12-13 16:10:20 -0600

    change "Media" objects properties name
    to be compatible with new "DataContext.js" file

--- js/models/media.js
+++ js/models/media.js
@@ -3,8 +3,8 @@ var app = window.app || {};
 app.Media = function(isbn, mediaType, name){
     "use strict";
 
-    this.isbn = isbn;
-    this.mediaType = mediaType;
-    this.name = name; 
+    this.ISBN = isbn;
+    this.MediaType = mediaType;
+    this.Name = name; 
 
 }
