32469727ea1bc227efdfee4a4204cff0613b7e42
Daniel Nieto <danielnieto89@gmail.com>
2016-12-13 16:16:15 -0600

    implement all logic for media.html
    allowing updating media items through the use of DataContext functions

--- js/viewModels/mediaViewModel.js
+++ js/viewModels/mediaViewModel.js
@@ -1,12 +1,43 @@
 var app = window.app || {};
 
-app.MediaViewModel = (function(){
+app.MediaViewModel = (function(ko, db){
     'use strict';
 
     var me = {
-        
+        catalog: ko.observableArray([]),
+        mediaTypes: ko.observableArray([]),
+        save: save
     }
+
+    function _getCatalog(){
+        db.getCatalog(function(data){
+             var tmpArray = [];
+
+             ko.utils.arrayForEach(data || [], function(item){
+                 console.log(">>>>" + JSON.stringify(item));
+                 tmpArray.push(new app.Media(item.ISBN, item.MediaType, item.Name));
+             });
+
+             me.catalog(tmpArray);
+        })
+    }
+
+    function _init(){
+        db.getMediaTypes(function(data){
+            me.mediaTypes(data);
+            _getCatalog();
+        });
+    }
+
+    function save(){
+        ko.utils.arrayForEach(me.catalog(), function(item){
+            console.log(item);
+            db.saveMedia(ko.toJS(item));
+        });
+    }
+
+    _init(); 
 
     return me;
 
-})();
+})(ko, app.DataContext);
