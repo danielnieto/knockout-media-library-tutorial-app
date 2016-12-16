32b070290a2f0ffd5b6d67e5d9d2ba0515d8b2fa
Daniel Nieto <danielnieto89@gmail.com>
2016-12-16 13:12:44 -0600

    use explicit chain of "contexts" to bind nested foreachs

--- js/viewModels/forEachViewModel.js
+++ js/viewModels/forEachViewModel.js
@@ -4,7 +4,13 @@ app.ForEachViewModel = (function(){
     'use strict';
 
     var me = {
-
+        products: [
+            { Name: 'Widget', Colors: ["Blue", "Yellow", "White"] },
+            { Name: 'Gadget', Colors: ["Blue", "Red", "Yellow"] },
+            { Name: 'Whatsit', Colors: ["Blue", "Green", "Yellow"] },
+            { Name: 'Whosit', Colors: ["Green", "Yellow", "Black"] },
+            { Name: 'Thingamabob', Colors: ["Blue", "Red", "White"] }
+        ]
     }
 
     return me;
