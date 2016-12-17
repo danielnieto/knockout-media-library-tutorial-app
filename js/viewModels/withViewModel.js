00378cdc004522a14b4bec5feeeef8bc6d0c5d19
Daniel Nieto <danielnieto89@gmail.com>
2016-12-17 12:16:35 -0600

    manage context for objects hierarchy using "with" binding

--- js/viewModels/withViewModel.js
+++ js/viewModels/withViewModel.js
@@ -3,9 +3,87 @@ var app = window.app || {};
 app.WithViewModel = (function(){
     'use strict';
 
-    var me = {
-
-    }
+        var me = {
+        taxonomies: [
+        {
+            Animal: {
+                Name: 'Cheetah',
+                Kingdom: {
+                    Name: 'Animalia',
+                    Phylum: {
+                        Name: 'Chordata',
+                        Class: {
+                            Name: 'Mammalia',
+                            Order: { 
+                                Name: 'Carnivora',
+                                Family: {
+                                    Name: 'Felidae',
+                                    Genus: {
+                                        Name: 'Acinynox',
+                                        Species: {
+                                            Name: 'Jubatus'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+        },
+        {
+            Animal: {
+                Name: 'Koala',
+                Kingdom: {
+                    Name: 'Animalia',
+                    Phylum: {
+                        Name: 'Chordata',
+                        Class: {
+                            Name: 'Mammalia',
+                            Order: {
+                                Name: 'Diprotondontia',
+                                Family: {
+                                    Name: 'Phascolarctidae',
+                                    Genus: {
+                                        Name: 'Phascolarctos',
+                                        Species: {
+                                            Name: 'Cinereus'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+        },
+        {
+            Animal: {
+                Name: 'Snowy Owl',
+                Kingdom: {
+                    Name: 'Animalia',
+                    Phylum: {
+                        Name: 'Chordata',
+                        Class: {
+                            Name: 'Aves',
+                            Order: {
+                                Name: 'Strigiformes',
+                                Family: {
+                                    Name: 'Strigidae',
+                                    Genus: {
+                                        Name: 'Nyctea',
+                                        Species: {
+                                            Name: 'Scandiaca'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }]
+    };
 
     return me;
 
