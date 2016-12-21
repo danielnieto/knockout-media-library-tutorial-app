b26e5210912b069a50d02201efd8ba89a3670ad3
Daniel Nieto <danielnieto89@gmail.com>
2016-12-21 13:03:19 -0600

    use inline <script> to implement a template
    this template is used to display 2 different types of media (books, and magazines) and it's using "as" property to "alias" the $data property to something else (to be used within the template)

--- js/viewModels/templateViewModel.js
+++ js/viewModels/templateViewModel.js
@@ -1,12 +1,34 @@
 var app = window.app || {};
 
-app.TemplateViewModel = (function(){
+app.TemplateViewModel = (function(ko, db){
     'use strict';
 
     var me = {
-
+        books: ko.observableArray([]),
+        magazines: ko.observableArray([])
     }
+
+    function _init(){
+        db.getCatalog(function (data) {
+            var b = [],
+                m = [];
+            ko.utils.arrayForEach(data, function (item) {
+                switch (item.MediaType) {
+                    case 'Book':
+                        b.push(item);
+                        break;
+                    case 'Magazine':
+                        m.push(item);
+                        break;
+                }
+            });
+            me.books(b);
+            me.magazines(m);
+        });
+    }
+
+    _init();
 
     return me;
 
-})();
+})(ko, app.DataContext);
