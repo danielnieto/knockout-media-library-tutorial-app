08958b5e70ae3f7feb38d1fe945983b45946871e
Daniel Nieto <danielnieto89@gmail.com>
2017-01-12 09:00:22 -0600

    use pureComputed properties to create a subset of borrowed and possessed items

--- js/models/media.js
+++ js/models/media.js
@@ -1,10 +1,12 @@
 var app = window.app || {};
 
-app.Media = function(isbn, mediaType, name){
+app.Media = function(isbn, mediaType, name, borrower, dueDate){
     "use strict";
 
     this.ISBN = isbn;
     this.MediaType = mediaType;
     this.Name = name; 
+    this.Borrower = ko.observable(borrower ? borrower : '');
+    this.DueDate = ko.observable(dueDate ? dueDate : undefined);
 
 }
