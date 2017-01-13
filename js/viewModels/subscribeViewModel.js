091a2099c006097d552f78437b214e5171a70b2b
Daniel Nieto <danielnieto89@gmail.com>
2017-01-13 14:41:37 -0600

    after selecting a borrowers display all their borrowed items in a separate list (using subscribe method)

--- js/viewModels/subscribeViewModel.js
+++ js/viewModels/subscribeViewModel.js
@@ -1,12 +1,34 @@
 var app = window.app || {};
 
-app.SubscribeViewModel = (function(){
+app.SubscribeViewModel = (function(ko, db){
     'use strict';
 
     var me = {
-
+        borrower: ko.observable(undefined),
+        borrowers: ko.observableArray([]),
+        catalog: ko.observableArray([]),
+        media: ko.observable(undefined),
+        previousBorrower: ko.observable(undefined)
     }
+
+    function _init() {
+      
+        db.getBorrowers(function (data) {
+            me.borrowers(data);
+        });
+
+        me.borrower.subscribe(function(v){
+            if(!v) return;
+
+            db.getBorrowedCatalog(v, function (data) { 
+                    me.catalog(data);
+                });
+
+        })
+    }
+
+    _init();
 
     return me;
 
-})();
+}(ko, app.DataContext));
