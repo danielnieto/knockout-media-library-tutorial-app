3226b779708a09ca416a0393856d5ff13f7f0d28
Daniel Nieto <danielnieto89@gmail.com>
2017-01-14 17:17:53 -0600

    use custom function to filter observableArray's items

--- js/viewModels/customFunctionViewModel.js
+++ js/viewModels/customFunctionViewModel.js
@@ -1,12 +1,26 @@
 var app = window.app || {};
 
-app.CustomFunctionViewModel = (function(){
+app.CustomFunctionViewModel = (function(ko, db){
     'use strict';
 
     var me = {
-
+        catalog: ko.observableArray([])
     }
+
+    function _init(){
+        db.getCatalog(function (data) {
+            me.catalog(data);
+        });
+        
+        me.books = me.catalog.filterByProperty('MediaType', 'Book');
+        me.cds = me.catalog.filterByProperty('MediaType', 'CD');
+        me.dvds = me.catalog.filterByProperty('MediaType', 'DVD');
+        me.games = me.catalog.filterByProperty('MediaType', 'Game');
+        me.magazines = me.catalog.filterByProperty('MediaType', 'Magazine');
+    }
+
+    _init();
 
     return me;
 
-})();
+})(ko, app.DataContext);
