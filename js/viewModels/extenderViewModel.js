b3c47893a38cb6c5d40f2e7dd16a169c654845e0
Daniel Nieto <danielnieto89@gmail.com>
2017-01-15 17:21:15 -0600

    display the list of borrowers on "extender" view

--- js/viewModels/extenderViewModel.js
+++ js/viewModels/extenderViewModel.js
@@ -1,12 +1,43 @@
 var app = window.app || {};
 
-app.ExtenderViewModel = (function(){
+app.ExtenderViewModel = (function(ko, db){
     'use strict';
-
+    
     var me = {
-        title: "Worx"
+        borrowers: ko.observableArray([]),
+        sortedBorrowers: undefined,
+        save: save
     }
+
+    function _getBorrowers(){
+        db.getBorrowers(function(data){
+             var tmpArray = [];
+
+             ko.utils.arrayForEach(data || [], function(item){
+                 tmpArray.push(new app.Borrower(item.Name, item.Email));
+             });
+
+             me.borrowers(tmpArray);
+        });
+    }
+
+    function _init(){
+        _getBorrowers();
+        me.sortedBorrowers =  ko.pureComputed(function () {
+            return this.borrowers().sort(function (l, r){
+                return (l.Name === r.Name ? 0 : l.Name > r.Name ? 1 : -1) || (l.Email === r.Email ? 0 : l.Email > r.Email ? 1 : -1);
+            });
+        }, me);
+    }
+
+    function save(){
+        ko.utils.arrayForEach(me.borrowers(), function(item){
+            db.saveBorrower(ko.toJS(item));
+        });
+    }
+
+    _init();
 
     return me;
 
-})();
+})(ko, app.DataContext);
