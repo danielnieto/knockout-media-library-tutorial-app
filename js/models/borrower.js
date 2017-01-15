0b9b1b62d50720656b5428b7c674c0d5e9332eeb
Daniel Nieto <danielnieto89@gmail.com>
2017-01-15 17:19:23 -0600

    make "borrower" model use "required" extender to validate its fields

--- js/models/borrower.js
+++ js/models/borrower.js
@@ -3,7 +3,10 @@ var app = window.app || {};
 app.Borrower = function(name, email){
     "use strict";
 
-    this.Name = name;
-    this.Email = email;
+    this.Name = ko.observable(name).extend({required: "Name is required"});
+    this.Email = ko.observable(email).extend({required: "Email is required"});
+    this.HasError = ko.pureComputed(function(){
+        return this.Name.hasError() || this.Email.hasError();
+    }, this);
 
 }
