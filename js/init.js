59506b9e57c14afbf15c0d985595890390b8fed4
Daniel Nieto <danielnieto89@gmail.com>
2017-01-21 13:37:20 -0600

    use durandal to display the page

---
+++ js/init.js
@@ -0,0 +1,21 @@
+﻿requirejs.config({
+    paths: {
+        'text': 'text',
+        'durandal': 'vendor/durandal',
+        'plugins': 'vendor/durandal/plugins',
+        'transitions': 'vendor/durandal/transitions',
+        'knockout': 'vendor/knockout-3.4.1',
+        'jquery': 'vendor/jquery-3.1.1'
+    }
+});
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
+    app.title = 'Welcome to Durandal';
+    app.configurePlugins({
+        router: true,
+        dialog: true
+    });
+    app.start().then(function () {
+        viewLocator.useConvention();
+        app.setRoot('viewModels/home', 'entrance');
+    });
+});
